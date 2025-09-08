7c69f6b312216b70c188d4df1ca265c1fa575270
Mara Becker <mara.becker@hamburger-software.de>
2025-09-08 12:49:48 +0200

    Car

---
+++ Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Engine
+{
+    public void Start()
+    {
+        Console.WriteLine("Engine started!");
+    }
+}
+
+public class Car
+{
+    private Engine _engine;
+
+    public Car()
+    {
+        _engine = new Engine(); // Abhängigkeit wird intern erzeugt
+    }
+
+    public void Start()
+    {
+        _engine.Start();
+        Console.WriteLine("Car is running.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.Start();
+    }
+}
